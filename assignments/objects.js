64df5de7d3e4a97470ba0a1d2524e76ab979f39a
Kaylen Milliner <kaylen.milliner@gmail.com>
2019-10-02 21:45:17 -0400

    Challenge 3

--- assignments/objects.js
+++ assignments/objects.js
@@ -31,6 +31,9 @@ const Kennan = {
   name: "Kennan",
   email: "kdiben1@tinypic.com",
   gender: "M",
+  speak: function(){
+    return `Hello, my name is ${this.name}!`;
+  }
 }
 
 const Keven = {
@@ -52,6 +55,10 @@ const Antonietta = {
   name: "Antonietta",
   email: "adaine5@samsung.com",
   gender: "F",
+  multiplyNums: function(num1, num2) {
+    return num1 * num2;
+  }
+
 }
 
 console.log(Mitzi, Kennan, Keven, Gannie, Antonietta);
@@ -79,8 +86,11 @@ console.log(Antonietta.gender);
 // ==== Challenge 3: Object Methods ==== 
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
 
+
+console.log(Kennan.speak());
+
 // Antonietta loves math, give her the ability to multiply two numbers together and return the product. Use the console.log provided as a hint.
-//console.log(antonietta.multiplyNums(3,4));
+console.log(Antonietta.multiplyNums(3,4));
 
 // === Great work! === Head over to the the arrays.js. You may come back and attempt the Stretch Challenge once you have completed the challenges in arrays.js and function-conversion.js.
 
